acdddd092e87bcc9cc6a0b71c2314b1f80bb8cf5
elsandb <107439377+elsandb@users.noreply.github.com>
2025-06-01 12:43:49 +0200

    feat/refactor: Divide `Header` into smaller components, and add theme toggle.

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -3,9 +3,9 @@ import type { Metadata } from "next";
 import Script from 'next/script';
 
 import SideNavBar from "@/components/navigation/SideNavBar";
-import Header from "@/components/navigation/Header";
-import HtmlThemeProvider from "../utils/html-theme-provider";
+import Header, { OffcanvasNavBar } from "@/components/navigation/Header";
 import Link from "next/link";
+import { ThemeProvider } from "@/utils/theme-provider";
 
 export const metadata: Metadata = {
   title: "Elsandb",
@@ -24,65 +24,65 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <HtmlThemeProvider>
+    <html lang="en">
       <body>
-        <div className="root-container container-fluid">
-          <div className="page-wrapper row pb-2">
-            {/* HEADER */}
-            <Header />
-            {/* col - SIDEBAR */}
-            <div id="sidebar" className="border-end col-3 collapse m-0 show">
-              <SideNavBar />
-            </div>
-            {/* col - MAIN */}
-            <div className="col d-flex pt-5 ps-0 pe-0">
-              <main className="w-100">
-                {children}
-              </main>
-            </div>
-          </div>
-        </div>
-
-        {/* FOOTER */}
-        <footer className="text-center bg-body-tertiary">
-          <div className="row pt-5 pb-5 m-0 justify-content-center">
-            {/* GitHub */}
-            <div className="col-sm-2">
-              <Link className="d-flex align-items-center justify-content-center gap-2"
-                href="https://github.com/elsandb"
-                target="_blank" rel="noopener noreferrer"
-                // style={{ color: "#538A90" }}
-              >
-                <i aria-hidden className="bi bi-github" />
-                GitHub
-              </Link>
-            </div>
-            {/* LinkedIn */}
-            <div className="col-sm-3">
-              <Link
-                className="d-flex align-items-center justify-content-center gap-2"
-                href="https://www.linkedin.com/in/elsandb/"
-                target="_blank" // open in a new window or tab
-                rel="external noopener noreferrer"
-                // style={{ color: "#538A90" }}
-              >
-                <i aria-hidden className="bi bi-linkedin" />
-                LinkedIn
-              </Link>
+        <ThemeProvider>
+          <div className="root-container container-fluid">
+            <div className="page-wrapper row pb-2">
+              <Header />
+              <div id="sidebar" className="border-end col-3 collapse m-0 show">
+                <SideNavBar />
+              </div>
+              <OffcanvasNavBar />
+              {/* col - MAIN */}
+              <div className="col d-flex pt-5 ps-0 pe-0">
+                <main className="w-100">
+                  {children}
+                </main>
+              </div>
             </div>
           </div>
-        </footer>
 
-        {/* <BootstrapClient /> */}
-        <Script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-          strategy="lazyOnload"
-          integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-          crossOrigin="anonymous"
-          async />
-        <script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/languages/go.min.js" async />
+          {/* FOOTER */}
+          <footer className="text-center bg-body-tertiary">
+            <div className="row pt-5 pb-5 m-0 justify-content-center">
+              {/* GitHub */}
+              <div className="col-sm-2">
+                <Link className="d-flex align-items-center justify-content-center gap-2"
+                  href="https://github.com/elsandb"
+                  target="_blank" rel="noopener noreferrer"
+                // style={{ color: "#538A90" }}
+                >
+                  <i aria-hidden className="bi bi-github" />
+                  GitHub
+                </Link>
+              </div>
+              {/* LinkedIn */}
+              <div className="col-sm-3">
+                <Link
+                  className="d-flex align-items-center justify-content-center gap-2"
+                  href="https://www.linkedin.com/in/elsandb/"
+                  target="_blank" // open in a new window or tab
+                  rel="external noopener noreferrer"
+                // style={{ color: "#538A90" }}
+                >
+                  <i aria-hidden className="bi bi-linkedin" />
+                  LinkedIn
+                </Link>
+              </div>
+            </div>
+          </footer>
 
+          {/* <BootstrapClient /> */}
+          <Script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+            strategy="lazyOnload"
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+            crossOrigin="anonymous"
+            async />
+          <script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/languages/go.min.js" async />
+        </ThemeProvider>
       </body>
-    </HtmlThemeProvider>
+    </html>
   );
 }
--- next-app/src/components/navigation/Header.tsx
+++ next-app/src/components/navigation/Header.tsx
@@ -2,80 +2,87 @@
 import { links } from "./links";
 import { NavLink } from "./NavLink";
 
+import { ThemeToggle } from "../ThemeToggle";
+
 export default function Header() {
   return (
-    <div id="navbar-top" className="row">
-      <div className="col">
-        {/* TOP AND OFFCANVAS NAVBAR */}
-        <nav className="navbar bg-body-tertiary fixed-top border-bottom">
-          <div className="container-fluid align-items-center">
+    <header className="border-bottom py-3 px-4 d-flex justify-content-between align-items-center">
+      <MenuButtons />
+      <SiteTitle />
+      <ThemeToggle />
+    </header>
+  );
+}
 
-            {/* Top navbar */}
-            <div className="d-flex align-items-center">
-              <div className="mr-auto">
-                <button
-                  id="offcanvas-toggle-btn"
-                  className="navbar-toggler"
-                  type="button"
-                  data-bs-toggle="offcanvas"
-                  data-bs-target="#offcanvasNavbar"
-                  aria-controls="offcanvasNavbar"
-                  aria-label="Toggle navigation"
-                >
-                  <span className="navbar-toggler-icon"></span>
-                </button>
-                <button
-                  id="sidebar-toggle-btn"
-                  className="navbar-toggler d-lg-block"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#sidebar"
-                  aria-controls="sidebar"
-                  aria-label="Toggle navigation"
-                >
-                  {/* <span className="navbar-toggler-icon" /> */}
-                  <span className="navbar-toggler-icon"></span>
-                </button>
-              </div>
-              {/* Logo */}
-              <a className="text-decoration-none" href="/">
-                <h4 className="ps-4 fw-light text-light-emphasis">
-                  @elsandb
-                </h4>
-              </a>
-            </div>
+function SiteTitle() {
+  return (
+    <a className="text-decoration-none fs-4 text-light-emphasis" href="/">
+      @elsandb
+    </a>
+  );
+}
 
-            {/* Offcanvas navbar */}
-            <div
-              id="offcanvasNavbar"
-              className="offcanvas offcanvas-start"
-              tabIndex={-1}
-              aria-labelledby="offcanvasNavbarLabel"
-            >
-              <div className="offcanvas-header">
-                {/* Logo */}
-                <h5 
-                  className="offcanvas-title text-light-emphasis" 
-                  id="offcanvasNavbarLabel">
-                  @elsandb
-                </h5>
-                {/* Button: close */}
-                <button 
-                  type="button" 
-                  className="btn-close" 
-                  data-bs-dismiss="offcanvas" 
-                  aria-label="Close" />
-              </div>
-
-              <div className="offcanvas-body">
-                <ul className="navbar-nav flex-grow-1 pe-3">
-                  {links.map((link)=><NavLink link={link} key={link.href}/>)}
-                </ul>
-              </div>
-            </div>
-          </div>
-        </nav>
+/**
+ * Menu toggle buttons for the side navbar and the offcanvas navbar.
+ */
+function MenuButtons() {
+  return (
+    <div className="d-flex align-items-center">
+      <div>
+        <button
+          id="offcanvas-toggle-btn"
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar"
+          aria-controls="offcanvasNavbar"
+          aria-label="Toggle navigation">
+          <span>
+            <svg width="24" height="24" stroke="currentColor" fill="none" strokeWidth="2">
+              <line x1="4" y1="6" x2="20" y2="6" />
+              <line x1="4" y1="12" x2="20" y2="12" />
+              <line x1="4" y1="18" x2="20" y2="18" />
+            </svg>
+          </span>
+        </button>
+        <button
+          id="sidebar-toggle-btn"
+          className="navbar-toggler d-lg-block"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#sidebar"
+          aria-controls="sidebar"
+          aria-label="Toggle navigation">
+          <span>
+            <svg width="24" height="24" stroke="currentColor" fill="none" strokeWidth="2">
+              <line x1="4" y1="6" x2="20" y2="6" />
+              <line x1="4" y1="12" x2="20" y2="12" />
+              <line x1="4" y1="18" x2="20" y2="18" />
+            </svg>
+          </span>
+        </button>
       </div>
     </div>
-  );
+  )
+}
+
+export function OffcanvasNavBar() {
+  return (
+    <div id="offcanvasNavbar"
+      className="offcanvas offcanvas-start"
+      tabIndex={-1}
+      aria-labelledby="offcanvasNavbarLabel">
+      <div className="offcanvas-header">
+        <h4 className="offcanvas-title text-light-emphasis ps-4" id="offcanvasNavbarLabel">
+          Menu
+        </h4>
+        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" />
+      </div>
+      <div className="offcanvas-body">
+        <ul className="navbar-nav flex-grow-1 pe-3">
+          {links.map((link) => <NavLink link={link} key={link.href} />)}
+        </ul>
+      </div>
+    </div>
+  )
 }
